9f57ad5d61639a7cbf60283753b7f6184560237a
Roman Loliwaffle <roman.loliwaffle@net.ik>
2024-02-14 11:43:38 +0300

    roma

---
+++ untitled/Hills/Alpinist.java
@@ -0,0 +1,10 @@
+package Hills;
+
+import java.util.Random;
+
+public class Alpinist {
+    public static void main(String[] args){
+    Random age = new Random();
+    int number = age.nextInt(70) + 10;
+    System.out.println("" + number);
+}}
